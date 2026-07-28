12ca83b568c092454850b397ffe0df24d2ad5cf4
Hermod <107561524+hermodgit@users.noreply.github.com>
2026-07-28 14:01:18 +0700

    Update index.ts

--- lib/api-client-react/src/index.ts
+++ lib/api-client-react/src/index.ts
@@ -4,3 +4,9 @@ export { setBaseUrl, setAuthTokenGetter } from "./custom-fetch";
 export type { AuthTokenGetter } from "./custom-fetch";
 export * from './generated/api';
 export * from './generated/api.schemas';
+export * from './triggers-api'
+export * from './agent-comms-api'
+export * from './webhooks-api';
+export * from './policy-templates-api';
+export * from './analytics-api';
+export * from './bug-bounty-api';
